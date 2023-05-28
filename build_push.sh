85ae749d828440556496b39a32e0ad76f68719bd
Aviv Shaar <avivshaar@Avivs-MacBook-Pro.local>
2023-05-28 22:57:25 +0300

    push omage to dockerhub

--- build_push.sh
+++ build_push.sh
@@ -2,6 +2,6 @@
 
 docker build -t cicd:latest .
 
-# docker tag cicd:latest aviv012/cicd:latest
+docker tag cicd:latest aviv012/cicd:latest
 
-# docker push aviv012/cicd:latest
+docker push aviv012/cicd:latest
